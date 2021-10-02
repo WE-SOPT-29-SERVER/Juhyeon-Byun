dee6e9314b252d69a4e94d7a60ce8e42c882a545
juhyeon <wngus4296@swu.ac.kr>
2021-10-02 16:08:30 +0900

    feat: null_undefined.js 생성

---
+++ 1st-seminar/null_undefined.js
@@ -0,0 +1,11 @@
+// null, undefined
+let nothing = null;
+console.log(`nothing : ${nothing}, type: ${typeof nothing}`); // object (자바스크립트의 첫 버전부터 있었지만, 그 때 당시에도 고치기엔 이미 너무 늦어버렸던(?)  버그. 10일만에 탄생한 언어라 어쩔 수 없다고 한다...)
+
+let x;
+console.log(`x: ${x}, type ${typeof x}`); // undefined
+
+// null vs undefined
+console.log("null vs undefined");
+console.log("null === undefined: ", null === undefined);
+console.log("null == undefined: ", null == undefined);
