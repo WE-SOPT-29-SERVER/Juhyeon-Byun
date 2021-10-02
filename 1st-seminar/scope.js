7c014589a614274b66b8c2ac1d4700bdf856ae31
juhyeon <wngus4296@swu.ac.kr>
2021-10-02 16:12:16 +0900

    feat scopt.js symbol.js variable.js 생성

---
+++ 1st-seminar/scope.js
@@ -0,0 +1,27 @@
+// Block Scope
+// var
+if (true) {
+    var x = "var";
+  }
+  console.log(`var: ${x}`);
+  
+  // let or const
+  if (true) {
+    let y = "let";
+    const z = "const";
+  }
+  console.log(`let: ${y}`);
+  console.log(`let: ${z}`);
+  
+  // Function Scope
+  // var is inaccessible from outside of the function
+  function colorFunction() {
+    if (true) {
+      var color = "blue";
+      console.log(color);
+    }
+    console.log(color);
+  }
+  
+  colorFunction();
+  console.log(color);
---
+++ 1st-seminar/variable.js
@@ -0,0 +1,39 @@
+// 재선언
+
+// var
+var variableVar = "123";
+var variableVar = "321";
+
+console.log("variableVar", variableVar);
+
+// let
+let variableLet = "123";
+let variableLet = "321";
+
+console.log("variableLet", variableLet);
+
+// const
+const variableConst = "123";
+const variableConst = "321";
+
+console.log("variableConst", variableConst);
+
+// 재할당
+
+// var
+var variableVar = "123";
+variableVar = "321";
+
+console.log("variableVar", variableVar);
+
+// let
+let variableLet = "123";
+variableLet = "321";
+
+console.log("variableLet", variableLet);
+
+// const
+const variableConst = "123";
+variableConst = "321";
+
+console.log("variableConst", variableConst);
