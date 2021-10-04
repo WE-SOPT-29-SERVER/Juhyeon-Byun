2a6a03714f37a7e79d042fd90917280f4586d8dd
juhyeon <wngus4296@swu.ac.kr>
2021-10-05 01:38:52 +0900

    feat: members 배열 생성 및 group별 인원수 출력  생성

--- 1st-assignment/random-group.js
+++ 1st-assignment/random-group.js
@@ -1,2 +1,54 @@
-// member.js파일을 이용하여 랜덤으로 조를 짜는 알고리즘 생성
 // 조건: OB와 YB 비율 오차범위를 최소화 한다.
+
+const members = [
+    { name: "강한희", part: "Server", group: "OB" },
+    { name: "고성용", part: "Server", group: "OB" },
+    { name: "구건모", part: "Server", group: "YB" },
+    { name: "권세훈", part: "Server", group: "YB" },
+    { name: "김영권", part: "Server", group: "YB" },
+    { name: "김은지", part: "Server", group: "YB" },
+    { name: "김진욱", part: "Server", group: "YB" },
+    { name: "김희빈", part: "Server", group: "OB" },
+    { name: "남지윤", part: "Server", group: "YB" },
+    { name: "문규원", part: "Server", group: "YB" },
+    { name: "박나희", part: "Server", group: "OB" },
+    { name: "박정현", part: "Server", group: "YB" },
+    { name: "박현지", part: "Server", group: "OB" },
+    { name: "변주현", part: "Server", group: "OB" },
+    { name: "서호영", part: "Server", group: "OB" },
+    { name: "설지원", part: "Server", group: "YB" },
+    { name: "손시형", part: "Server", group: "YB" },
+    { name: "안준영", part: "Server", group: "OB" },
+    { name: "장서현", part: "Server", group: "OB" },
+    { name: "오예원", part: "Server", group: "OB" },
+    { name: "이다은", part: "Server", group: "OB" },
+    { name: "이동근", part: "Server", group: "YB" },
+    { name: "이솔", part: "Server", group: "OB" },
+    { name: "이승헌", part: "Server", group: "YB" },
+    { name: "이정은", part: "Server", group: "YB" },
+    { name: "이제준", part: "Server", group: "YB" },
+    { name: "정설희", part: "Server", group: "OB" },
+    { name: "조윤서", part: "Server", group: "OB" },
+    { name: "조재호", part: "Server", group: "YB" },
+    { name: "조찬우", part: "Server", group: "YB" },
+    { name: "주어진사랑", part: "Server", group: "YB" },
+    { name: "주효식", part: "Server", group: "YB" },
+    { name: "채정아", part: "Server", group: "OB" },
+    { name: "최영재", part: "Server", group: "OB" },
+    { name: "최유림", part: "Server", group: "YB" },
+    { name: "최진영", part: "Server", group: "YB" },
+    { name: "허유정", part: "Server", group: "YB" },
+];
+
+console.log(members.length);
+let ybNum = 0;
+let obNum = 0;
+members.forEach(member => {
+    if (member.group === "YB") {
+        ybNum++;
+    }
+    else {
+        obNum++;
+    }
+});
+console.log(ybNum, obNum);
