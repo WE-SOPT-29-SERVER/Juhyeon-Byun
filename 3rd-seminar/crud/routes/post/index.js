5b7e6a408ee2df287ed5a333004a5c26ba81517b
juhyeon <wngus4296@swu.ac.kr>
2021-10-27 12:55:30 +0900

    [FIX] post/index.js 수정

--- 3rd-seminar/crud/routes/post/index.js
+++ 3rd-seminar/crud/routes/post/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router()
 
 router.get("/", require("./postAllGET"));
-// router.get("/:id", require("./postOneGET"));
+router.get("/:id", require("./postOneGET"));
 // router.post("/", require("./postCreatePOST"));
 // router.put("/:id", require("./postUpdatePUT"));
 // router.delete("/:id", require("./postOneDELETE"));
