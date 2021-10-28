bc0a00e4e530c02dff42973e964da3f388408e90
juhyeon <wngus4296@swu.ac.kr>
2021-10-28 17:19:06 +0900

    [FIX] post delete, update 라우터 주석 해제

--- 3rd-seminar/crud/routes/post/index.js
+++ 3rd-seminar/crud/routes/post/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 router.get("/", require("./postAllGET"));
 router.get("/:id", require("./postOneGET"));
 router.post("/", require("./postCreatePOST"));
-// router.put("/:id", require("./postUpdatePUT"));
-// router.delete("/:id", require("./postOneDELETE"));
+router.put("/:id", require("./postUpdatePUT"));
+router.delete("/:id", require("./postOneDELETE"));
 
 module.exports = router;
